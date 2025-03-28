6f94380a5f9bbe60d4b048193cffd2c8148be91f
unknown <lekicjovan1212@gmail.com>
2025-03-28 19:31:46 +0100

    refactor code

--- src/components/pages/home/TrendingSection/slider/trendingMovieCard/Index.jsx
+++ src/components/pages/home/TrendingSection/slider/trendingMovieCard/Index.jsx
@@ -1,15 +1,16 @@
 import { CiBookmark, CiSearch } from "react-icons/ci";
+import { useNavigate } from "react-router";
 
 function TrendingMovieCard(props) {
-  const { image } = props;
-
+  const { image,imdbid } = props;
+let navigate = useNavigate()
   return (
     <article className="trending__movie__card">
       <img src={image} alt="" className="trending__movie__img" />;
       <div className="bookmark__div">
         <CiBookmark className="bookmark__icon" />
       </div>
-      <button className="see__more__btn">
+      <button className="see__more__btn" onClick={()=>navigate(`movie/${imdbid}`)}>
         See more
         <CiSearch className="search__icon" />
       </button>
